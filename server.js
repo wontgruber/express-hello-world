4d42a74065700de501b434aaf690792bc8ff474f
Nick Wilgruber <nwilgruber@gmail.com>
2023-04-15 17:54:30 -0400

    Playing

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const app = require('./app')
+const app = require("./app");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/appointments", (req, res) => {
+    res.send("Getting appointments...");
+    res.end();
+});
+
+app.post("/appointments", (req, res) => {
+    res.send("Posting appointments");
+    res.send(req);
+
+    // 1 route
+    // Post comes in from the webhook
+    res.end();
+});
